5e0daae6f4c5b57302ac21d11de7a2e078cd4e6e
“expertnovice1” <expertnovice1@gmail.com>
2023-11-10 18:09:00 +0000

    initial commit

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+    backend "gcs" {
+    bucket  = "roidtc-projects-215-tf-state"
+    prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.2.0"
+    }
+  }
+}
+
+
+provider "google" {
+
+  project = var.project_id
+
+}
